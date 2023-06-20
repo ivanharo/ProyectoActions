b6fe4e5008a7e1f4d2d8f0a6d6f8d36709d0893b
ivanharo <43594421+ivanharo@users.noreply.github.com>
2023-06-20 15:42:53 -0600

    Update ejecuta.py

--- .github/workflows/ejecuta.py
+++ .github/workflows/ejecuta.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
   
 for renglon in open('archi.xml'):
   aux = []
- partes = renglon.split()[0]
+ partes = renglon.split()
   if partes[0] == '<Componente':
     print(renglon.split()[1])
   else 
