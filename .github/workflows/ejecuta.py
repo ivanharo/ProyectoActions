98dfff5624edc1d90b14646403e75baea9073fe3
ivanharo <43594421+ivanharo@users.noreply.github.com>
2023-06-20 02:16:11 -0600

    leeArchivoXML

--- .github/workflows/ejecuta.py
+++ .github/workflows/ejecuta.py
@@ -1,5 +1,12 @@
-def saludo():
-    print("Hola mundo desde python")
-saludo()
-  
+import xml.etree.ElementTree as ET
 
+try:
+    xml_file = open('archi.xml')         
+    if xml_file.readable():
+        print(True)
+    else:
+        print(False)
+except Exception as err:
+    print("Error:",err)
+finally:
+    xml_file.close()
