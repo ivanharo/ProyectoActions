209cfea3c9c0fdce4bb1cc7f173399c41534e17d
ivanharo <43594421+ivanharo@users.noreply.github.com>
2023-06-20 09:02:42 -0600

    ultimo

--- .github/workflows/ejecuta.py
+++ .github/workflows/ejecuta.py
@@ -4,10 +4,10 @@ try:
     xml_file = open('archi.xml')         
     if xml_file.readable():
         xml_data = ET.fromstring(xml_file.read())
-        lst_plants = xml_data.findall('PLANT')
+        lst_plants = xml_data.findall('remite')
         print(lst_plants)
         for plant in lst_plants:
-            print(f"Dato: {plant.find('componente').text}")
+            print(f"Dato: {plant.find('nombre').text}")
     else:
         print(False)
 except Exception as err:
