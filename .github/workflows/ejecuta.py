63570e35e60046be3904d834e1c42794498e1a18
ivanharo <43594421+ivanharo@users.noreply.github.com>
2023-06-20 16:06:49 -0600

    Update ejecuta.py

--- .github/workflows/ejecuta.py
+++ .github/workflows/ejecuta.py
@@ -1,8 +1,7 @@
-
-  for renglon in open('archi.xml'):
-    aux = []
-    partes = renglon.split()
-    if partes[0] == '<Componente':
-      print(renglon.split()[1])
-    else 
-      print('no se encontro')
+for renglon in open('archi.xml'):
+  aux = []
+  partes = renglon.split()
+  if partes[0] == '<Componente':
+    print(renglon.split()[1])
+  else 
+    print('no se encontro')
