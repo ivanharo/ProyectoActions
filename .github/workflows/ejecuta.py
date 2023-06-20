93aab4b536091d407cfab8d466415d0d66ab7bf0
ivanharo <43594421+ivanharo@users.noreply.github.com>
2023-06-20 11:49:40 -0600

    read

--- .github/workflows/ejecuta.py
+++ .github/workflows/ejecuta.py
@@ -1,16 +1,3 @@
-import xml.etree.ElementTree as ET
-
-try:
-    xml_file = open('archi.xml')         
-    if xml_file.readable():
-        xml_data = ET.fromstring(xml_file.read())
-        lst_plants = xml_data.findall('aplicacion')
-        print(lst_plants)
-        for plant in lst_plants:
-            print(f"Dato: {plant.find('Componente').text}")
-    else:
-        print(False)
-except Exception as err:
-    print("Error:",err)
-finally:
-    xml_file.close()
+ xml_file = open("archi.xml","r") 
+print(xml_file.read())
+f.close()
