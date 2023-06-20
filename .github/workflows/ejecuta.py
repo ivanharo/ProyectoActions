a274f7e7ae6baa8e59738880c4f1db6938363c42
ivanharo <43594421+ivanharo@users.noreply.github.com>
2023-06-20 16:20:05 -0600

    Update ejecuta.py

--- .github/workflows/ejecuta.py
+++ .github/workflows/ejecuta.py
@@ -2,6 +2,6 @@ for renglon in open('archi.xml'):
   aux = []
   partes = renglon.split()
   if partes[0] == '<Componente':
-    print(renglon.split()[1,2,3,4])
+    print(renglon.split()[1]renglon.split()[2]renglon.split()[3])
   else: 
     print('no se encontro')
